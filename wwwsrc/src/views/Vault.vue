a895930f5eab1c3dc25faf837778c8fa81a0be7e
Rhianon Kilzer <rhianonkilzer@yahoo.com>
2018-10-11 11:35:22 -0600

    vault construction

--- wwwsrc/src/views/Vault.vue
+++ wwwsrc/src/views/Vault.vue
@@ -1,9 +1,42 @@
 <template>
+    <div class="vault">
+        <h3>
+            {{vault.name}}
+        </h3>
 
+
+    </div>
 </template>
 <script>
     export default {
         name: "vault",
+        // data() {
+        //     return {
+
+        //     }
+        // }
+        created() {
+            if (!this.$store.state.user._id) {
+                this.$router.push({ name: "login" });
+            }
+        },
+        computed: {
+            vault() {
+                return this.$store.state.activeVault
+            },
+            keeps() {
+                return this.$store.state.keeps
+            }
+        },
+        mounted() {
+            this.$store.dispatch("getVault", this.vaultId)
+            this.$store.dispatch("getKeeps", this.vaultId)
+        },
+        methods: {
+            addKeep() {
+                this.$store.dispatch("")
+            }
+        }
 
     }
 
